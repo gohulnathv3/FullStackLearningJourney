0168d07462c4cf4feae9d1420227d56a0ed178e3
jeyapriyagohul <jeyapriyagohul@gmail.com>
2023-05-14 01:57:39 +0530

    Added code changes for variable

---
+++ Frontend/JavaScript/Sample/Basics/Variables2.js
@@ -0,0 +1,8 @@
+const address = "7,339 PudhuNagar, emaneswaram India";
+
+var name = "Gohul Nath V";
+var email = "gohul.va@gmail.com ";
+var password = "12234"
+var common = Password
+var courseCount = "123343";
+var 
