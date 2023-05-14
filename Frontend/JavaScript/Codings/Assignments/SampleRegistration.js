daf7fdc9bc1c8628b2c0c367f1184983f025a02e
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-15 01:05:23 +0530

    Added Day1 Assignments and organized folders

---
+++ Frontend/JavaScript/Codings/Assignments/SampleRegistration.js
@@ -0,0 +1,19 @@
+var firstName = "Gohul";
+var lastName = "Nath V"
+var userEmail = "gohulnathv.dev@gmail.com";
+var userAddress = "7/330, Pudhu Nagar, Paramakudi"
+var isLoggedFromGoogle = "No";
+var gender = "Male";
+var country = "India";
+
+console.log("Signed up successfully using below credentials: ");
+
+console.log(`
+    First Name: ${firstName}
+    Last Name: ${lastName}
+    Mail: ${userEmail}
+    Address: ${userAddress}
+    Gender: ${gender}
+    Country: ${country}
+    LoggedFromGoogle: ${isLoggedFromGoogle}
+`);
---
+++ Frontend/JavaScript/Codings/Basics/Variables2.js
@@ -0,0 +1,24 @@
+const userId = "123";
+
+var fullName = "Gohul Nath V";
+var email = "gohul.va@gmail.com";
+var password = "12345";
+var confirmPassword = "12345";
+var courseCount = 0;
+var isLoggedFromGoogle = false;
+
+
+// fullName = prompt("Enter your name"); - to get the input from user
+
+
+console.log(userId);
+console.log("full name is ", fullName);
+console.log(email);
+
+// other ways of printing variables
+console.log(`
+    Unique ID: ${userId}
+    Username is: ${fullName}
+    and his email is:${email}
+    and the password is: ${password}
+`); 
