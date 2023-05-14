0a5557bb2208e794d4a63547526613094ae2c07c
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-15 02:54:13 +0530

    Added functions

---
+++ Frontend/JavaScript/Codings/Intermediate/functions.js
@@ -0,0 +1,12 @@
+// With Paramaters
+function sayName(name) {
+  console.log(`Welcome ${name}, Hope you're enjoying JS functions`);
+}
+sayName("Gohul");
+
+// Without Parameters
+function welcomeIndia() {
+  return "Welcome to Incredible India";
+}
+var welcomeMessage = welcomeIndia();
+console.log(welcomeMessage);
