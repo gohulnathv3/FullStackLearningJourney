a6b7f3d00597d669de744512d63c17c26b73b2ca
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-15 02:31:20 +0530

    Added ConditionalRelated use cases

---
+++ Frontend/JavaScript/Codings/Basics/ConditionalSignOut.js
@@ -0,0 +1,13 @@
+var authenticated = false;
+
+// if(authenticated){
+//     console.log("Show signout button");
+// } else {
+//     console.log("Show login options")
+// }
+
+
+// go through this to make code clean https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+
+var result = authenticated ? "Show signout button" : " Show login options";
+console.log(result);
---
+++ Frontend/JavaScript/Codings/Basics/ConditionalsRoles.js
@@ -0,0 +1,25 @@
+// Create applicatin with various roles admin, testadmin, subadmin, user
+
+var user = "subadmin";
+
+switch (user) {
+    case "admin":
+        console.log("You got the full access");
+        break;
+    
+        case "testadmin":
+        console.log("You got the access of Tests");
+        break;
+
+        case "subadmin":
+        console.log("You got the access of create/delete  user");
+        break;
+
+        case "user":
+        console.log("You got the course access");
+        break;
+
+        default:
+        console.log("Trial user");
+        break;
+}
