975f3a8d39a637f6f73f0b38a3a566d695b16680
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-16 01:12:21 +0530

    ScopeChaining Assignment

---
+++ Frontend/JavaScript/Codings/Assignments/ScopeChaining.js
@@ -0,0 +1,13 @@
+function outer(){
+     var outer = "I'm outside variable";
+     function inner(){
+        var inner = "I'm inside variable";
+        console.log(outer+", "+inner);
+     }
+     inner();
+}
+outer();
+
+// In this example, the inner function is nested within the outer function. 
+// When the outer function is called, it creates a scope that includes the variable outer variable and defines the inner function within that scope.
+// When the inner function is called, it has access to both outer variable and inner variable.
