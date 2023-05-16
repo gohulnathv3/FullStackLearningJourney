8dded66defb8593b5809b4fa26885a350e237e5d
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-17 03:53:19 +0530

    Code Formatting

--- Frontend/JavaScript/Codings/Assignments/FilteringOtherTypes.js
+++ Frontend/JavaScript/Codings/Assignments/FilteringOtherTypes.js
@@ -1,15 +1,15 @@
 // TODO: filtering out other types in the array
 
-let countries = ["Canada","America","India",1947, true, "United Kingdom"];
+let countries = ["Canada", "America", "India", 1947, true, "United Kingdom"];
 // using continue keyword -> It is used to skip the rest of code if the codition is true and continue iteration
-for (let i = 0; i < countries.length; i++){
-    if( typeof countries[i] == "string") continue;
-    console.log(countries[i]);
+for (let i = 0; i < countries.length; i++) {
+  if (typeof countries[i] == "string") continue;
+  console.log(countries[i]);
 }
 
 // using break keyword -> It is used to break the loop and exit.
 console.log("Break Keyword Logic started");
-for (let i =0 ; i < countries.length ; i++ ){
-    if(countries[i]==true) break;
-    console.log(countries[i]);
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i] == true) break;
+  console.log(countries[i]);
 }
