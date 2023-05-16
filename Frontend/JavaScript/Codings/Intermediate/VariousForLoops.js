48781d34ebd8108e2979c683005d9b296edb6448
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-17 05:03:12 +0530

    Code Formatting

--- Frontend/JavaScript/Codings/Intermediate/VariousForLoops.js
+++ Frontend/JavaScript/Codings/Intermediate/VariousForLoops.js
@@ -4,7 +4,7 @@ let countries = ["Canada", "America", "India", 1947, true, "United Kingdom"];
 console.log("Iteration Array elements using for each");
 countries.forEach((s) => console.log(s));
 
-// interview question 1 
+// interview question 1
 console.log("Interview Question Output");
 var i = 0;
 for (;;) {
@@ -14,23 +14,23 @@ for (;;) {
 }
 
 console.log("For Of Loop Output");
-for(const country of countries){
-    console.log(country);
+for (const country of countries) {
+  console.log(country);
 }
 console.log("For in Loop Output 1");
-for(const country in countries){
-    console.log(countries[country]);
+for (const country in countries) {
+  console.log(countries[country]);
 }
 
 console.log("For in Loop Output 2");
 // if key value pairs are there in an array, we've to use for..in loop
 const symbols = {
-    yt: "youtube",
-    ig: "instagram",
-    fb: "facebook",
-    sc: "snapchat"
-}
+  yt: "youtube",
+  ig: "instagram",
+  fb: "facebook",
+  sc: "snapchat",
+};
 
-for (const symbol in symbols){
-    console.log(symbol+": "+symbols[symbol]);
+for (const symbol in symbols) {
+  console.log(symbol + ": " + symbols[symbol]);
 }
