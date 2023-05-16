f9a17ea9e8797ed89c822dd1bd20741299334311
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-17 01:50:29 +0530

    This Keyword part 1

---
+++ Frontend/JavaScript/Codings/Intermediate/ThisKeyword.js
@@ -0,0 +1,11 @@
+// TODO Lesson 1 for this keyword
+console.log(this); // if you run the same in browser, it will throw "undefined" error.  It will show Window
+
+var game = "FIFA";
+
+// function sayName(){
+//     var name = "Gohul";
+//     console.log(this);
+// }
+
+// sayName();
