02112f04a75e44f01f5bfb7678bbfb9bb2bfc7e7
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-17 01:50:18 +0530

    Array Slice and Splice

---
+++ Frontend/JavaScript/Codings/Intermediate/SliceArray.js
@@ -0,0 +1,7 @@
+var countries = ["Japan", "Canada", "America", "India", "UK"];
+console.log(countries.slice(1,4));
+console.log(countries.slice(1));
+
+// splice 
+countries.splice(1,3,"Hi","BYE")
+console.log(countries);
