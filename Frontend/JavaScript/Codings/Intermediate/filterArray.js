2a1658b124e6e28fcfa5d83868a02ca26ca7f5eb
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-17 01:50:05 +0530

    Array Filter

---
+++ Frontend/JavaScript/Codings/Intermediate/filterArray.js
@@ -0,0 +1,7 @@
+var testArray = [2, 4, 6, 7, 8, 3, 4];
+
+console.log(testArray.fill("h", 1, 4));
+
+const number = [13, 4, 5, 55, 6, 732, 23, 43, 23, 1, 12, 54];
+const result = number.filter((num) => num < 23);
+console.log(result);
