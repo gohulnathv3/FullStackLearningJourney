ab098f772877bff646a65675a276a3eda1aa2801
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-18 23:45:59 +0530

    Code Formatting

--- Frontend/JavaScript/Codings/DOM/01dom/DOM.js
+++ Frontend/JavaScript/Codings/DOM/01dom/DOM.js
@@ -4,14 +4,22 @@
 // The DOM represents each element of an HTML or XML document as an object, and these objects are organized in a tree structure, with the document object at the root.
 // Each element, such as <div>, <p>, or <span>, is represented by a node in the DOM tree.
 
-// The DOM provides a set of properties, methods, and events that allow JavaScript code to interact with the HTML or XML document. 
+// The DOM provides a set of properties, methods, and events that allow JavaScript code to interact with the HTML or XML document.
 // It serves as a bridge between web content and scripting languages like JavaScript, enabling dynamic web page interactions and modifications.
 
+// Here's a simple example that demonstrates accessing and modifying an element using the DOM:
+
 // Accessing an element by its ID
-const element = document.getElementById('myElement');
+const element = document.getElementById("myElement");
 
 // Modifying the element's content
-element.textContent = 'Hello, World!';
+element.textContent = "Hello, World!";
 
 // Modifying the element's style
-element.style.color = 'red';
+element.style.color = "red";
+
+// In this example, we access an element with the ID 'myElement' using getElementById().
+//  Then, we modify its content using textContent and change its color using style.color.
+
+// The DOM is an essential part of web development with JavaScript,
+// allowing you to create interactive and dynamic web pages by manipulating and updating the content and appearance of the document.
