43fb7b59ce13c2ef9578a5a1ede06c8ae138ef75
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-18 23:25:01 +0530

    DOM JS

---
+++ Frontend/JavaScript/Codings/DOM/01dom/DOM.js
@@ -0,0 +1,8 @@
+// Accessing an element by its ID
+const element = document.getElementById('myElement');
+
+// Modifying the element's content
+element.textContent = 'Hello, World!';
+
+// Modifying the element's style
+element.style.color = 'red';
