1dffef7687ff3a9fa979162e287c9306e968e60b
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-20 23:56:20 +0530

    Removed App Components

--- Frontend/JavaScript/Codings/Projects/MyReactFirstApp/my-first-react-app/src/App.js
+++ Frontend/JavaScript/Codings/Projects/MyReactFirstApp/my-first-react-app/src/App.js
@@ -2,24 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to this page
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by using this original documentation
-        </a>
-      </header>
-    </div>
-  );
+  return null;
 }
 
 export default App;
