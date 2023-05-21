2ebefdc40eda02a3995a8db83c26b3dfb75da834
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-21 23:00:38 +0530

    Event Listener Demo (script.js Added)

---
+++ Frontend/JavaScript/Codings/DOM/02EventListener/script.js
@@ -0,0 +1,10 @@
+// to get the button element by using getElementById
+const clickme = document.getElementById("click-me");
+
+// to add the event listener for this button
+
+clickme.addEventListener("click", function () {
+  // code to be executed for specific logic if we click this clickme button
+  alert("button clicked!");
+//   console.log("Button Clicked");
+});
