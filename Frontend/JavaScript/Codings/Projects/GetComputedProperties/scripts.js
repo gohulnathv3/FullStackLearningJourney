010d8425f1eeab53235046934975ccf3732c9c8a
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-21 22:43:58 +0530

    Modified the project 2 - Have doubts

---
+++ Frontend/JavaScript/Codings/Projects/GetComputedProperties/scripts.js
@@ -0,0 +1,8 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".red");
+const violet = document.querySelector(".red");
+const orange = document.querySelector(".red");
+const pink = document.querySelector(".red");
+
+const center = document.querySelector(".center");
+console.log(window.getComputedStyle(red).backgroundColor);
