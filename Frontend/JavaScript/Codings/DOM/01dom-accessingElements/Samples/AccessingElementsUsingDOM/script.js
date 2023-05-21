cdee0c5f0d9afda3451e75c689164f7babc1e5fe
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-22 00:58:57 +0530

    Sample Program for Accessing Elements

---
+++ Frontend/JavaScript/Codings/DOM/01dom-accessingElements/Samples/AccessingElementsUsingDOM/script.js
@@ -0,0 +1,15 @@
+// Accessing the element by ID
+var titleElement = document.getElementById("title");
+titleElement.innerHTML = "Hello, JavaScript!";
+
+// Accessing elements by class name
+var paragraphs = document.getElementsByClassName("highlight");
+for (var i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "blue";
+}
+
+// Accessing the element by tag name
+var button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", function () {
+  alert("Button clicked!");
+});
