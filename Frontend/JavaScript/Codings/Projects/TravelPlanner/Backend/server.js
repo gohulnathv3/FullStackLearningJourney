342808a107ce21b6c415ff02fdb47c6d0cb66823
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-25 00:50:13 +0530

    Built basic API calls using Node.js/Express.js

--- Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
+++ Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
@@ -1,12 +1,29 @@
-const express = require('express')
+const express = require('express');
 
-// create an Express application
+// Create an Express application
 const app = express();
 const port = 3000;
 
-// define API routes
-app.get('/',(req,res) => {
-    // handle get request for fetching users
-    // use your logic below
-    res.json({message: 'Get all users'});
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Define API routes
+app.get('/', (req,res) => {
+  res.send('Hello, world!');
+});
+
+// Example API route to handle GET request
+app.get('/api/users', (req, res) => {
+    // Replace this with your actual implementation
+    const users = [
+      { id: 1, name: 'Gohul Nath V' },
+      { id: 2, name: 'Pavithran R' }
+    ];
+    res.json(users);
+  });
+
+  // Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
