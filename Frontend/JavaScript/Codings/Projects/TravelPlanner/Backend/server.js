062fd087b8de05ac4abdcacc40b77a62c0937bdc
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-26 02:47:47 +0530

    Basic MongoDB Connection

--- Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
+++ Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
@@ -1,55 +1,35 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-// Mongo DB URL Connection
-const url = "mongodb://localhost:27017";
-// Database and collection name
-const dbName = "travel_app_db";
-const destinationCollection = "destinations";
-// To create a Mongo Client
-const client = new MongoClient(url, {
-  userNewParser: true,
-  useUnifiedTopology: true,
-});
-// Connect to MongoDB database
-client.connnect((err) => {
-  if (err) {
-    console.error("Failed to connect to the DB: ", err);
-    return;
+const { MongoClient } = require('mongodb');
+
+// MongoDB connection URL
+const url = 'mongodb://localhost:27017';
+const dbName = 'StudentRepo';
+
+// Function to connect to MongoDB
+async function connectToMongoDB() {
+  try {
+    // Create a new MongoClient
+    const client = new MongoClient(url, { useUnifiedTopology: true });
+
+    // Connect to the MongoDB server
+    await client.connect();
+
+    console.log('Connected to MongoDB');
+
+    // Access the database
+    const db = client.db(dbName);
+
+    // Perform database operations
+    const collection = db.collection("StudentRepo");
+    const data = await collection.find({}).toArray();
+    console.log(data);
+
+    // Close the database connection
+    client.close();
+    console.log('Disconnected from MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
   }
-  console.log("Connected to the database");
-});
+}
 
-// Create an Express application
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON request bodies
-app.use(express.json());
-
-// Define API routes
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
-// Example API route to handle GET request
-app.get("/api/users", (req, res) => {
-  // Replace this with your actual implementation
-  const users = [
-    { id: 1, name: "Gohul Nath V" },
-    { id: 2, name: "Pavithran R" },
-  ];
-  res.json(users);
-});
-
-// Example API route to handle POST request
-app.post("/api/users", (req, res) => {
-  // Replace this with your actual implementation
-  const { name } = req.body;
-  const newUser = { id: 3, name };
-  res.json(newUser);
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Call the function to connect to MongoDB
+connectToMongoDB();
