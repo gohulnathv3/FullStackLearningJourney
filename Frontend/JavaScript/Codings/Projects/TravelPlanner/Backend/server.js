783c930c8d982f2c2fa72a532f0b8c34eefc3a91
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-25 23:52:12 +0530

    Added DBs and collection name

--- Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
+++ Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const express = require("express");
+const { MongoClient } = require("mongodb");
 // Mongo DB URL Connection
-const url = 'mongodb://localhost:27017';
-
+const url = "mongodb://localhost:27017";
+// Database and collection name
+const dbName = "travel_app_db";
+const destinationCollection = "destinations";
 
 // Create an Express application
 const app = express();
 const port = 3000;
 
-
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Define API routes
-app.get('/', (req,res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 // Example API route to handle GET request
-app.get('/api/users', (req, res) => {
-    // Replace this with your actual implementation
-    const users = [
-      { id: 1, name: 'Gohul Nath V' },
-      { id: 2, name: 'Pavithran R' }
-    ];
-    res.json(users);
-  });
+app.get("/api/users", (req, res) => {
+  // Replace this with your actual implementation
+  const users = [
+    { id: 1, name: "Gohul Nath V" },
+    { id: 2, name: "Pavithran R" },
+  ];
+  res.json(users);
+});
 
 // Example API route to handle POST request
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   // Replace this with your actual implementation
   const { name } = req.body;
   const newUser = { id: 3, name };
@@ -37,5 +38,5 @@ app.post('/api/users', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
