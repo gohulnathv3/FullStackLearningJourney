6ca6696fee2bcec0d420b7796648239fb35a7196
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-27 22:56:17 +0530

    Changed to different port

--- Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/db-server.js
+++ Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/db-server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
