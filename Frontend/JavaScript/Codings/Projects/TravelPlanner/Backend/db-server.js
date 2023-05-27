45e02a1c96e80f7f0484ab82058327528d0f6ed6
jeyapriyagohul <64803321+gohulnathv3@users.noreply.github.com>
2023-05-27 23:41:13 +0530

    Removing duplicate code by creating common func

--- Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/db-server.js
+++ Frontend/JavaScript/Codings/Projects/TravelPlanner/Backend/db-server.js
@@ -11,80 +11,46 @@ app.use(express.json());
 const url = "mongodb://localhost:27017";
 const dbName = "travel_app_db";
 
-// API route to retrieve data from the collection
-app.get("/api/v1/destinations", async (req, res) => {
+// function to estabilish connection
+async function connectToDatabase() {
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  const db = client.db(dbName);
+  return { client, db };
+}
+
+// Route handler for retrieving data from a collection
+async function retrieveDataFromCollection(req, res, collectionName) {
   try {
-    const collectionName = "destinations";
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(dbName);
+    const { client, db } = await connectToDatabase();
     const collection = db.collection(collectionName);
-
     const data = await collection.find({}).toArray();
-
     client.close();
-
     res.json(data);
   } catch (error) {
-    console.error("Failed to fetch data:", error);
+    console.error("Failed to fetch the data", error);
     res.status(500).json({ error: "Failed to fetch data" });
   }
+}
+
+// API routes for retrieving data from collection
+
+app.get("/api/v1/destinations", async (req, res) => {
+  retrieveDataFromCollection(req, res, "destinations");
 });
 
 // to retrieve users
 app.get("/api/v1/users", async (req, res) => {
-  try {
-    const collectionName = "users";
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const data = await collection.find({}).toArray();
-
-    client.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
+  retrieveDataFromCollection(req, res, "users");
 });
 
 // to retrieve reviews collections
 app.get("/api/v1/reviews", async (req, res) => {
-  try {
-    const collectionName = "reviews";
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const data = await collection.find({}).toArray();
-
-    client.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
+  retrieveDataFromCollection(req, res, "reviews");
 });
 
 // to retrive itineraries collections
 app.get("/api/v1/itineraries", async (req, res) => {
-  try {
-    const collectionName = "users";
-    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-
-    const data = await collection.find({}).toArray();
-
-    client.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
+  retrieveDataFromCollection(req, res, "itineraries");
 });
 
 // Start the server
